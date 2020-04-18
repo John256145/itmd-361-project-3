0768abb8a0b75387df85ec888a356c57c0418035
Juan Gonzalez <john256145@gmail.com>
2020-04-18 14:27:04 -0500

    added marker data to js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var marker;
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.70275, lng: -87.535264},
@@ -84,4 +86,19 @@ function initMap() {
             }
           ]
     });
+
+    marker = new google.maps.Marker({
+      map: map,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      position: {lat: 59.327, lng: 18.067}
+    });
+    marker.addListener('click', toggleBounce);
+}
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
